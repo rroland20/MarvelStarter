1c66c76f66e407c063ebbf030bc26ff8a0772206
rroland20 <alena_tumanova96@mail.ru>
2023-06-15 14:36:47 +0400

    final

---
+++ src/components/charInfo/CharInfo.js
@@ -0,0 +1,128 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import './charInfo.scss';
+import MarvelService from '../../services/MarvelService';
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+import Skeleton from '../skeleton/Skeleton';
+
+class CharInfo extends Component {
+
+    state = {
+        char: null,
+        loading: false,
+        error: false
+    }
+
+    marvelService = new MarvelService();
+
+    updateChar = () => {
+        const {charId} = this.props;
+        if (!charId)
+            return;
+        
+        this.onCharLoading();
+        this.marvelService
+            .getCharacter(charId)
+            .then(this.onCharLoaded)
+            .catch(this.onError)
+    }
+
+    componentDidMount() {
+        this.updateChar();
+    }
+
+    componentDidUpdate(prevProps) {
+        if (this.props.charId !== prevProps.charId) {
+            this.updateChar()
+        }
+    }
+
+    onCharLoaded = (char) => {
+        this.setState({char, loading: false});
+    }
+
+    onCharLoading = () => {
+        this.setState({
+            loading: true
+        })
+    }
+
+    onError = () => {
+        this.setState({
+            loading: false,
+            error: true
+        })
+    }
+
+    render() {
+        const {char, loading, error} = this.state;
+        const skeleton = char || loading || error ? null : <Skeleton />;
+        const errorMessage = error ? <ErrorMessage /> : null;
+        const spinner = loading ? <Spinner /> : null;
+        const content = !(loading || error || !char) ? <View char={char} /> : null;
+
+        return (
+            <div className="char__info">
+                {skeleton}
+                {errorMessage}
+                {spinner}
+                {content}
+            </div>
+        )
+    }
+}
+
+const View = ({char}) => {
+    const {name, description, thumbnail, homepage, wiki, comics} = char;
+    let style = {objectFit: 'cover'};
+    
+    if (char.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        style = {objectFit: 'unset'};
+    }
+
+    return (
+        <>
+            <div className="char__basics">
+                <img src={thumbnail} alt={name} style={style}/>
+                <div>
+                    <div className="char__info-name">{name}</div>
+                    <div className="char__btns">
+                        <a href={homepage} className="button button__main">
+                            <div className="inner">homepage</div>
+                        </a>
+                        <a href={wiki} className="button button__secondary">
+                            <div className="inner">Wiki</div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div className="char__descr">
+                {description}
+            </div>
+            <div className="char__comics">Comics:</div>
+            <ul className="char__comics-list">
+                {comics.lenght > 0 ? null : 'There is no comics with this character'}
+                {
+                    comics.map((item, i) => {
+                        if (i < 10) {
+                            return (
+                                <li key={i} className="char__comics-item">
+                                    {item.name}
+                                </li>
+                            );
+                        }
+                        return null;
+                    })
+                }
+            </ul>
+        </>
+    );
+}
+
+CharInfo.propTypes = {
+    charId: PropTypes.number
+}
+
+export default CharInfo;
---
+++ src/components/charList/CharList.js
@@ -0,0 +1,110 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import './charList.scss';
+
+import MarvelService from '../../services/MarvelService'; 
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+
+class CharList extends Component {
+
+    state = {
+        chars: [],
+        loading: true,
+        error: false,
+        newItemLoading: false,
+        offset: 210,
+        charEnded: false
+    };
+
+    marvelService = new MarvelService();
+
+    componentDidMount() {
+        this.onRequest();
+    }
+
+    onCharLoaded = (newChars) => {
+        let ended = false;
+        if (newChars.lenght < 9) {
+            ended = true;
+        }
+        this.setState(({offset, chars}) => ({
+            chars: [...chars, ...newChars],
+            loading: false,
+            newItemLoading: false,
+            offset: offset + 9,
+            charEnded: ended
+        }));
+    }
+
+    onCharListLoading = () => {
+        this.setState({
+            newItemLoading: true
+        });
+    }
+
+    onError = () => {
+        this.setState({error: true, loading: false});
+    }
+
+    onRequest = (offset) => {
+        this.onCharListLoading();
+        this.marvelService
+            .getAllCharacters(offset)
+            .then(this.onCharLoaded)
+            .catch(this.onError);
+    }
+
+    listItem = (chars) => {
+        const items = chars.map(char => {
+            let style = {objectFit: 'cover'};
+    
+            if (char.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+                style = {objectFit: 'unset'};
+            }
+            return (
+                <li className="char__item"
+                    key={char.id}
+                    onClick={() => this.props.onCharSelected(char.id)}>
+                        <img style={style} src={char.thumbnail} alt={char.name}/>
+                        <div className="char__name">{char.name}</div>
+                </li>
+            );
+        });
+        return (
+            <ul className="char__grid">
+                    {items}
+                </ul>
+        );
+    }
+
+    render() {
+        const {chars, loading, error, newItemLoading, offset, charEnded} = this.state;
+        const items = this.listItem(chars);
+        const errorMessage = error ? <ErrorMessage /> : null;
+        const spinner = loading ? <Spinner /> : null;
+        const view = !(loading || error) ? items : null;
+        
+        return (
+            <div className="char__list">
+                    {errorMessage}
+                    {spinner}
+                    {view}
+                <button 
+                    className="button button__main button__long"
+                    disabled={newItemLoading}
+                    style={{display: charEnded ? 'none' : 'block'}}
+                    onClick={() => this.onRequest(offset)}>
+                    <div className="inner">load more</div>
+                </button>
+            </div>
+        )
+    }
+}
+
+CharList.propTypes = {
+    onCharSelected: PropTypes.func.isRequired
+}
+
+export default CharList;
